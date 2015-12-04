c5f945778272578e3056482d065f1cafebe02107
Joshua Chan <jchan3580@gmail.com>
2015-12-03 21:35:48 -0500

    minor inventory changes

--- PAttributes.mli
+++ PAttributes.mli
@@ -7,8 +7,14 @@ val gain_xp : player -> int -> player
 (*Takes in the experience of the player and computes the level*)
 val level_calc : player -> player
 
+(*Remove helper function*)
+val add_item : item list -> string -> unit
+
 (*Remove an item from the player inventory*)
-val remove_item : player -> string -> unit
+val remove_item : item list -> string -> unit
+
+(*Checks whether or not an item is in the inventory*)
+val in_inv : item list -> string -> bool
 
 (*Changes the inventory of the player*)
 val use_item : player -> player
